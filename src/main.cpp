e545f6f41d62629608aa111033d6bfeace33abc3
James Richer <richerjames@gmail.com>
2025-12-05 17:24:48 +0000

    basic window creation and writing to pixels set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <cmath>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Image.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
 
 int main() {
-    std::cout << "Hello world from CMake!" << std::endl;
+    int width = 150;
+    int height = 150;
+    sf::RenderWindow window(
+        sf::VideoMode(sf::Vector2u(width,height), 8),
+        "Demo Window"
+    );
+
+    sf::Clock frame_clock;
+    sf::Clock global_clock;
+
+    const float targetFPS = 30.0f;
+    const sf::Time frame_time = sf::seconds(1.0f/targetFPS);
+
+    unsigned char* pixels = new unsigned char[width*height*4];
+    for (int i=0; i < width*height*4; i++) {
+        pixels[i] = 255;
+    }
+
+    sf::Texture fb_texture(sf::Vector2u(width, height));
+    sf::Sprite fb_sprite(fb_texture);
+
+    while(window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+        float time = global_clock.getElapsedTime().asSeconds();
+        float brightness = (1.0f + std::sin(time)) * 127.0;
+        for (int i=0; i < width*height*4; i++) {
+            pixels[i] = brightness;
+        }
+
+        fb_texture.update(pixels);
+        window.draw(fb_sprite);
+
+        window.display();
+
+        sf::Time elapsed = frame_clock.getElapsedTime();
+        if (elapsed < frame_time) {
+            sf::sleep(frame_time - elapsed);
+        }
+        frame_clock.restart();
+    }
+
+    delete[] pixels;
 
     return 0;
 }
